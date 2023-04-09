b8b75b3fbd484ee1848e35f99702fc75a86cdc89
MD Abdullah AL Mamun <47145788+ronssrony@users.noreply.github.com>
2023-04-10 00:17:49 +0600

    Update 2d rotate.cpp

--- 2d tranformation/2d rotate.cpp
+++ 2d tranformation/2d rotate.cpp
@@ -1,1 +1,53 @@
 
+#include <GL/glut.h>
+
+void init(void)
+{
+    
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+
+    
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+}
+
+void display(void)
+{
+    
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    glColor3f(1.0, 0.0, 0.0);
+
+    glRotatef(90,0,0,1) ;//changing the value in first points to rotate ;
+    glBegin(GL_TRIANGLES);
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(0.5, -0.5);
+    glVertex2f(0.0, 0.5);
+    glEnd();
+
+    
+    glFlush();
+}
+
+int main(int argc, char** argv)
+{
+   
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(250, 250);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Triangle");
+
+    
+    init();
+
+    
+    glutDisplayFunc(display);
+
+    
+    glutMainLoop();
+
+    return 0;
+}
